c162f2d6b1a2e2168e231cbca84cb16a1dac5e8a
tazmaniax <chris@spinthewebb.com>
2012-08-09 17:55:43 +0200

    Update js/bootstrap-toggle.js
    Passed the checkbox back in the onClick hander so it can interrogated directly

--- js/bootstrap-toggle.js
+++ js/bootstrap-toggle.js
@@ -37,7 +37,7 @@
     
     // Setup Click
     this.$element.click(function (e) {
-      if(self.options.onClick) self.options.onClick(e, self.$checkbox.is(':checked'))
+      if(self.options.onClick) self.options.onClick(e, self.$checkbox, self.$checkbox.is(':checked'))
       self.toggle()
     });
   }
